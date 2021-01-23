6b7609555f77d32b3987b9a741044748a3644d74
unknown <ravijot1212@gmail.com>
2021-01-23 16:53:11 +0530

    changes made to the project

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const linkrouter = require('./routers/route.js')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT||4400
-const publicdir = path.join(__dirname, './public')
+const publicdir = path.join(process.cwd(), './public')
 app.set('view engine','hbs')
 app.use(cors())
 app.use(express.static(publicdir))
