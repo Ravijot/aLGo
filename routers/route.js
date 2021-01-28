613aa34ff6b4f65345de3eabdcc292f945affdd6
unknown <ravijot1212@gmail.com>
2021-01-28 10:36:43 +0530

    K

--- routers/route.js
+++ routers/route.js
@@ -4,7 +4,7 @@ const router = new express.Router()
 const app = express()
 
 var fs = require('fs')
-ext = '.TXT'
+ext = '.txt'
 pic = '.JPG'
 router.get('/',(req,res) => {
     res.sender('./public/index.html')
